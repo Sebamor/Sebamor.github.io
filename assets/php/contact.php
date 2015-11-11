8d852bb72c4d23ab5a127bbdfc9438b1b321c37b
Sebamor <sebamor12@gmail.com>
2015-11-11 14:55:54 -0700

    first commit

---
+++ assets/php/contact.php
@@ -0,0 +1,33 @@
+<?php
+
+if(isset($_POST['message'])){
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+    
+	
+	$to      = 'sebamor12@gmail.com';
+	$subject = 'Amore Portfolio Contact';
+
+	$headers = 'From: '. $email . "\r\n" .
+    'Reply-To: '. $email . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+	$status = mail($to, $subject, $message, $headers);
+
+	if($status == TRUE){	
+		$res['sendstatus'] = 'done';
+	
+		//Edit your message here
+		$res['message'] = 'Submission Successful';
+    }
+	else{
+		$res['message'] = 'Failed to send! Contact me anyways: Sebamor12@gmail.com';
+	}
+	
+	
+	echo json_encode($res);
+}
+
+?>
